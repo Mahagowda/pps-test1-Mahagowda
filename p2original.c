f0080870389f87a783eb9ffa112b51ece3045051
Mahagowda <94665876+Mahagowda@users.noreply.github.com>
2021-12-03 08:18:56 +0000

    pass by value

---
+++ p2original.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int input()
+{
+  int a;
+  printf("enter the number:");
+  scanf("%d",&a);
+  return a;
+}
+int find_sum(int a,int b)
+{
+  int sum;
+  sum=a+b;
+  return sum;
+}
+int output(int a,int c,int sum)
+{
+  printf("sum of %d+%d is %d\n",a,c,sum);
+  return 0;
+}
+int main()
+{
+  int x,y,z;
+  x=input();
+  y=input();
+  z=find_sum(x,y);
+  output(x,y,z);
+}
