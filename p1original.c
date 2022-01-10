51cdc294469b3b21722e837d8b9f795b7045331c
Mahagowda <94665876+Mahagowda@users.noreply.github.com>
2022-01-10 15:53:53 +0530

    Update p1original.c

--- p1original.c
+++ p1original.c
@@ -1,2 +1,11 @@
-
+#include<stdio.h>
+int main()
+{
+  int num1,num2,res;
+  printf("enter the number\n");
+  scanf("%d%d",&num1,&num2);
+  res=num1+num2;
+  printf("addition of two numbers:%d\n",res);
+  return 0;
+}
 
