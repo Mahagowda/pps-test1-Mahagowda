776b2d9517d48e56f38feb40cb60f56cd9b594bf
Mahagowda <94665876+Mahagowda@users.noreply.github.com>
2022-01-16 10:18:11 +0530

    Update p3final.c

---
+++ p3final.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+void input(int*a,int*b)
+{
+printf("enter the number:");
+scanf("%d%d",a,b);
+}
+void add(int a,int b,int*sum)
+{
+*sum=a+b;
+}
+void output(int a, int c, int sum)
+{
+printf("sum of %d+%d is %dn",a,c,sum);
+}
+int main()
+{
+int a,b,sum;
+input(&a,&b);
+add(a,b,&sum);
+output(a,b,sum);
+return 0;
+}
