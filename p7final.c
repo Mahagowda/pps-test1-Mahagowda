9531a6f861ec76268ad911bd7102491a1b64713c
Mahagowda <94665876+Mahagowda@users.noreply.github.com>
2022-01-19 16:56:32 +0530

    Update p7final.c

---
+++ p7final.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int number,i,sum=0;
+printf("\n enter any integer value\n");
+scanf("%d",&number;i++)
+{
+sum=sum+i;
+}
+printf("sum of natueal number=%d",sum);
+return 0;
+}
