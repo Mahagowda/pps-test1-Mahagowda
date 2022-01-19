c8a2a0fce36f231fd15bf944ae79e55b2bfe7f01
Mahagowda <94665876+Mahagowda@users.noreply.github.com>
2022-01-19 16:51:14 +0530

    Update p7original.c

--- p7original.c
+++ p7original.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+int main()
+{
+  int number,i,sum=0;
+  printf("\n enter any integer value\n");
+  scanf("%d",& number);
+  for(i=1;i<=number;i++)
+  {
+    sum=sum+i;
+  }
+  printf("sum of natural number=%d",sum);
+  return 0;
+}
